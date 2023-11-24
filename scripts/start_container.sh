658e6986ace1bc0cd1379f06792cf59243e16df6
GDVINODH <144826280+GDVINODH@users.noreply.github.com>
2023-11-24 21:34:13 +0530

    add the files for code deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull vinodh2580/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 vinodh2580/simple-python-app
+
