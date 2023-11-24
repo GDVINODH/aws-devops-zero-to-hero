32e650c4b943c0377fa310d12cb50861f5c66415
GDVINODH <144826280+GDVINODH@users.noreply.github.com>
2023-11-24 22:41:50 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -e
 
-# Stop and remove all Docker containers if there are any
-CONTAINERS=$(docker ps -aq)
-
-if [ -n "$CONTAINERS" ]; then
-  docker stop $CONTAINERS || true
-  docker rm $CONTAINERS || true
-  echo "All Docker containers stopped and removed."
-else
-  echo "No Docker containers found to stop and remove."
-fi
+# Stop the running container (if any)
+echo "Hi"
